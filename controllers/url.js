5963a2b8ba94c311c16d949db248944ff96060dc
Adiiiikaushik <adityakaushik1801@gmail.com>
2024-09-15 02:10:07 +0530

    Updated Analytics

--- controllers/url.js
+++ controllers/url.js
@@ -10,7 +10,10 @@ async function handleGenerateNewShortURL(req,res) {
         redirectURL: body.url,
         visitHistory: [],
     });
-    return res.json({id: shortID });
+
+    return res.render("home", {
+        id: shortID,
+    });
 }
 
 async function handleGetAnalytics(req,res) {
--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const {connectToMongoDB} = require("./connect")
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const URL = require("./models/url");
+const path = require("path");
 
 const app = express();
 const PORT = 8001;
 
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(()=> console.log("MongoDB connected."));
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
 app.use("/url", urlRoute);
-app.get("/:shortId", async(req,res) => {
+app.use("/", staticRoute);
+
+app.get("/url/:shortId", async(req,res) => {
     const shortId = req.params.shortId;
     const entry = await  URL.findOneAndUpdate({
         shortId
